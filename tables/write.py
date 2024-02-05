d138b95ab173912b8189cc742dcb1aaf139624df
hill <hill@hilll.dev>
2024-02-05 12:48:07 +0100

    tables

--- tables/write.py
+++ tables/write.py
@@ -5,23 +5,25 @@ if __name__ == "__main__":
     helper.write_ratios_to_file("12tet", 12)
     helper.write_ratios_to_file("24tet", 24)
     helper.write_ratios_to_file("6tet", 6)
-    helper.write_ratios_to_file("5tet", 5)
-    tones = [0] * 128
-    with open("overtones", "w") as file:  
-        for i in range(128):
+
+    i = 128 
+
+    tones = [0] * i 
+    with open(f"overtones_{i}", "w") as file:  
+        for i in range(i):
             power =  helper.next_lower_power_of_2(i)
             ratio_str = str(i) + "/" + str(power)
             tone = (int(i),ratio_str, i / power)
             tones[i] = tone
             file.write(f"{tone[0]}: {tone[2]}: {tone[1]}\n")
         
-        file.write(f"\n\n\n")
+        file.write("\n\n\n")
 
         sorted_tones = sorted(tones, key=lambda x: x[2])
         for tone in sorted_tones:
             file.write(f"{tone[0]}: {tone[2]}: {tone[1]}\n")
 
-        file.write(f"\n\n\n")
+        file.write("\n\n\n")
 
 
         unique_values_set = set()
@@ -35,12 +37,8 @@ if __name__ == "__main__":
         for tone in deduplicated_tones:
             file.write(f"{tone[0]}: {tone[2]}: {tone[1]}\n")
 
-        file.write(f"\n\n\n")
+        file.write("\n\n\n")
 
         for i in range(len(deduplicated_tones)):
             if i % 2 != 0:
                 file.write(f"{deduplicated_tones[i][0]} ")
-
-        file.write(f"\n\n\n")
-        file.write(f"const just_intonation_24: FractionTable = \n")
-
