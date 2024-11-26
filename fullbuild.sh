2c76f57b586a0a504395de3ac668e3e5fd191f88
hill <hill@hilll.dev>
2024-11-26 07:36:05 +0000

    npm

--- fullbuild.sh
+++ fullbuild.sh
@@ -2,17 +2,19 @@
 
 rm -rf content/piano/debug content/piano/tuningplayground/
 rm -rf tuningplayground/www tuningplayground/www-dev
-cd tuningplayground
-echo "building master"
-./build.sh prod
-rm ./www/chords.json | true
-rm ./www/163.bootstrap.js.LICENSE.txt | true
-mv ./www ./stable
-echo "building dev"
-./build.sh dev
-rm ./www/chords.json | true
-mv ./www ./debug
-mv ./debug ../content/piano/
-mv ./stable ../content/piano/tuningplayground/
+          cd tuningplayground
+          sh lint.sh
+          echo "building master"
+          ./build.sh prod
+          rm ./www/163.bootstrap.js.LICENSE.txt | true
+          mv ./www ./stable
+          echo "building dev"
+          ./build.sh dev
+          rm ./www/chords.json | true
+          rm ./www/chords.txt | true
+          mv ./www ./debug
+          mv ./debug ../content/piano/
+          mv ./stable ../content/piano/tuningplayground/
+          mv ../content/piano/tuningplayground/chords.* ../content/piano/
 cd ..
 sh build.sh
