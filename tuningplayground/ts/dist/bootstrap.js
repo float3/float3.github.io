b2063801c06652a3bb76fe8dbebeaaac5960577a
hill <hill@hilll.dev>
2024-11-26 04:20:50 +0000

    .gitignores

--- tuningplayground/ts/dist/bootstrap.js
+++
@@ -1,3 +0,0 @@
-﻿"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-import("./index.js").catch((e) => console.error("Error importing `index.js`:", e));
--- tuningplayground/ts/dist/config.js
+++
@@ -1,4 +0,0 @@
-﻿"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.midiMultiplier = void 0;
-exports.midiMultiplier = 1000;
