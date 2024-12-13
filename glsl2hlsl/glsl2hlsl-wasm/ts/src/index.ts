5f605ac1aab9ae05d435b1d9322d5e0b8cf159d6
hill <hill@hilll.dev>
2024-12-13 16:01:45 +0000

    use let instead of const

--- glsl2hlsl/glsl2hlsl-wasm/ts/src/index.ts
+++ glsl2hlsl/glsl2hlsl-wasm/ts/src/index.ts
@@ -13,14 +13,14 @@ import("glsl2hlsl-wasm").then((module) => {
   wasm.main()
 })
 
-const inp = document.getElementById("in") as HTMLTextAreaElement
-const outp = document.getElementById("out") as HTMLTextAreaElement
-const shader = document.getElementById("shader") as HTMLInputElement
-const raymarch = document.getElementById("raymarch") as HTMLInputElement
-const extract = document.getElementById("extract") as HTMLInputElement
+let inp = document.getElementById("in") as HTMLTextAreaElement
+let outp = document.getElementById("out") as HTMLTextAreaElement
+let shader = document.getElementById("shader") as HTMLInputElement
+let raymarch = document.getElementById("raymarch") as HTMLInputElement
+let extract = document.getElementById("extract") as HTMLInputElement
 
-const convertButton = document.getElementById("convert") as HTMLButtonElement
-const downloadButton = document.getElementById("download") as HTMLButtonElement
+let convertButton = document.getElementById("convert") as HTMLButtonElement
+let downloadButton = document.getElementById("download") as HTMLButtonElement
 
 convertButton.addEventListener("click", () => {
   if (inp && outp && extract && raymarch) {
@@ -30,16 +30,16 @@ convertButton.addEventListener("click", () => {
 
 downloadButton.addEventListener("click", () => {
   if (shader && extract && raymarch) {
-    const arr = shader.value.split("/").filter((x) => x.length > 0)
+    let arr = shader.value.split("/").filter((x) => x.length > 0)
 
-    const xhttp = new XMLHttpRequest()
+    let xhttp = new XMLHttpRequest()
     xhttp.onload = function () {
       if (this.responseText) {
         wasm.download(this.responseText, extract.checked, raymarch.checked)
       }
     }
 
-    const shaderId = arr[arr.length - 1]
+    let shaderId = arr[arr.length - 1]
     if (shaderId) {
       xhttp.open("GET", `https://www.shadertoy.com/api/v1/shaders/${shaderId}?key=NtHtMm`)
       xhttp.send()
@@ -49,8 +49,8 @@ downloadButton.addEventListener("click", () => {
 
 let textFile: string | null = null
 
-const makeTextFile = (text: string): string => {
-  const data = new Blob([text], { type: "text/plain" })
+let makeTextFile = (text: string): string => {
+  let data = new Blob([text], { type: "text/plain" })
 
   if (textFile !== null) {
     window.URL.revokeObjectURL(textFile)
@@ -65,7 +65,7 @@ window.downloadFile = downloadFile
 window.downloadImage = downloadImage
 
 export function downloadFile(name: string, contents: string): void {
-  const a = document.createElement("a")
+  let a = document.createElement("a")
   a.style.display = "none"
   a.href = makeTextFile(contents)
   a.download = name
@@ -75,10 +75,10 @@ export function downloadFile(name: string, contents: string): void {
 }
 
 export function downloadImage(name: string, contents: string): void {
-  const c = document.createElement("br")
+  let c = document.createElement("br")
   document.querySelector("#links")?.appendChild(c)
 
-  const a = document.createElement("a")
+  let a = document.createElement("a")
   a.innerHTML = name
   a.href = contents
   a.download = name
@@ -90,7 +90,7 @@ export function downloadImage(name: string, contents: string): void {
 }
 
 export function reset(): void {
-  const links = document.querySelector("#links")
+  let links = document.querySelector("#links")
   if (links) {
     links.innerHTML = "<p></p><h2>Textures (Ctrl+Click and Save-As):</h2><br>"
   }
