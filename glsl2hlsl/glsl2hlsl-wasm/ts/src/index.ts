65e33fe6e0f82f7eaf2b4037581476da7e5b6fdc
hill <hill@hilll.dev>
2024-12-13 16:09:33 +0000

    try to fix downloading

--- glsl2hlsl/glsl2hlsl-wasm/ts/src/index.ts
+++ glsl2hlsl/glsl2hlsl-wasm/ts/src/index.ts
@@ -1,13 +1,5 @@
 export let wasm: typeof import("glsl2hlsl-wasm")
 
-declare global {
-  interface Window {
-    downloadFile: (name: string, contents: string) => void
-    downloadImage: (name: string, contents: string) => void
-    reset: () => void
-  }
-}
-
 import("glsl2hlsl-wasm").then((module) => {
   wasm = module
   wasm.main()
@@ -46,52 +38,3 @@ downloadButton.addEventListener("click", () => {
     }
   }
 })
-
-let textFile: string | null = null
-
-let makeTextFile = (text: string): string => {
-  let data = new Blob([text], { type: "text/plain" })
-
-  if (textFile !== null) {
-    window.URL.revokeObjectURL(textFile)
-  }
-
-  textFile = window.URL.createObjectURL(data)
-  return textFile
-}
-
-window.reset = reset
-window.downloadFile = downloadFile
-window.downloadImage = downloadImage
-
-export function downloadFile(name: string, contents: string): void {
-  let a = document.createElement("a")
-  a.style.display = "none"
-  a.href = makeTextFile(contents)
-  a.download = name
-  document.body.appendChild(a)
-  a.click()
-  document.body.removeChild(a)
-}
-
-export function downloadImage(name: string, contents: string): void {
-  let c = document.createElement("br")
-  document.querySelector("#links")?.appendChild(c)
-
-  let a = document.createElement("a")
-  a.innerHTML = name
-  a.href = contents
-  a.download = name
-  document.querySelector("#links")?.appendChild(a)
-
-  document.body.appendChild(a)
-  a.click()
-  document.body.removeChild(a)
-}
-
-export function reset(): void {
-  let links = document.querySelector("#links")
-  if (links) {
-    links.innerHTML = "<p></p><h2>Textures (Ctrl+Click and Save-As):</h2><br>"
-  }
-}
