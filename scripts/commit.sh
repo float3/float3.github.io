7b5fd9816b9d088a28a8be84f788c10f34b4c6c9
hill <hill@hilll.dev>
2024-12-14 15:30:13 +0000

    refactor all code out of workflow files

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -4,5 +4,5 @@ git config --global user.email "github-actions[bot]@users.noreply.github.com"
 git config --global user.name "github-actions[bot]"
 git add -A
 git pull
-git diff --staged --quiet || git commit -m "generate"
+git diff --staged --quiet || git commit -m $GITHUB_JOB
 git push || echo "No changes to commit"
