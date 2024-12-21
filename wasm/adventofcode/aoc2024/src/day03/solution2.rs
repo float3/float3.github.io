d66d6b36d7bfe6e3ebd2294a3873c075457b4a3c
hill <hill@hilll.dev>
2024-12-21 15:17:37 +0000

    aoc work

--- wasm/adventofcode/aoc2024/src/day03/solution2.rs
+++ wasm/adventofcode/aoc2024/src/day03/solution2.rs
@@ -1,3 +1,30 @@
-pub fn solve(_input: &str) -> String {
-    todo!()
+use super::solution1::{parse_instruction, Instruction};
+
+pub fn solve(input: &str) -> String {
+    let mut sum = 0;
+    let mut mul_enabled = true;
+
+    let mut index = 0;
+    while index < input.len() {
+        if let Some((instr, next_idx)) = parse_instruction(input, index) {
+            match instr {
+                Instruction::Mul(x, y) => {
+                    if mul_enabled {
+                        sum += x * y;
+                    }
+                }
+                Instruction::Do => {
+                    mul_enabled = true;
+                }
+                Instruction::Dont => {
+                    mul_enabled = false;
+                }
+            }
+            index = next_idx;
+        } else {
+            index += 1;
+        }
+    }
+
+    sum.to_string()
 }
