72678c7011b5af27726f1e209c4a0b823c8ca40d
hill <hill@hilll.dev>
2024-12-21 01:43:58 +0000

    aoc2024day1problem2

--- wasm/adventofcode/aoc2024/src/day01/solution2.rs
+++ wasm/adventofcode/aoc2024/src/day01/solution2.rs
@@ -1,3 +1,15 @@
-pub fn solve(_input: &str) -> String {
-    todo!()
+pub fn solve(input: &str) -> String {
+    let (left, right): (Vec<i32>, Vec<i32>) = input
+        .trim()
+        .lines()
+        .map(|line| {
+            let (x, y) = line.split_once("   ").unwrap();
+            (x.parse::<i32>().unwrap(), y.parse::<i32>().unwrap())
+        })
+        .unzip();
+
+    left.into_iter()
+        .map(|x| x * (right.iter().filter(|&&y| y == x).count() as i32))
+        .sum::<i32>()
+        .to_string()
 }
