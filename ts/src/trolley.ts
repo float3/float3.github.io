b44868ffc79f9cf65f1a7a6c6112d0f502d14918
hill <hill@hilll.dev>
2024-12-21 17:14:44 +0000

    Update trolley.ts

--- ts/src/trolley.ts
+++ ts/src/trolley.ts
@@ -4,8 +4,11 @@ document.addEventListener("DOMContentLoaded", async () => {
   const extensions = ["mp4", "jpg"]
   const randomNum = Math.floor(Math.random() * NUM) + 1
 
-  const mp4Src = `${trolleyPath}${randomNum}.${extensions[0]}`
-  const jpgSrc = `${trolleyPath}${randomNum}.${extensions[1]}`
+  const randomNum = String(randomNumber).padStart(2, '0')
+
+// Construct the file paths using the two-digit random number
+const mp4Src = `${trolleyPath}${randomNum}.${extensions[0]}`
+const jpgSrc = `${trolleyPath}${randomNum}.${extensions[1]}`
 
   const response = await fetch(jpgSrc)
   const selectedSrc = response.ok ? jpgSrc : mp4Src
