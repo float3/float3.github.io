95d69367914d1df8058162b8fcaea978bdadc44b
hill <hill@hilll.dev>
2025-01-05 23:12:44 +0000

    remove outdated Python scripts and add a new Bash script for date management in Markdown files

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,6 +3,8 @@
 ./scripts/collect_links.sh
 ./scripts/indices.sh
 
+CD=$(pwd)
+
 cd ./wasm/tuningplayground/music21-rs/
 
 python -m venv venv
@@ -11,3 +13,6 @@ pip install --upgrade pip
 pip install -r music21/requirements.txt
 pip install music21
 python -m generate_chords
+
+cd $CD/content
+../scripts/dates.sh
