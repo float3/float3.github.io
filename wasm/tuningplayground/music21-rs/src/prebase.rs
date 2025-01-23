4d7c48c46c6d3380d8d882c33ff762c94a36932a
hill <hill@hilll.dev>
2025-01-23 16:06:35 +0000

    merge

--- wasm/tuningplayground/music21-rs/src/prebase.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Clone, PartialEq, PartialOrd, Eq, Ord, Debug)]
-pub struct ProtoM21Object {}
-
-impl ProtoM21Object {
-    pub fn new() -> Self {
-        ProtoM21Object {}
-    }
-}
--- wasm/tuningplayground/music21-rs/src/specifier.rs
+++
@@ -1,6 +0,0 @@
-#[derive(Clone, Debug)]
-pub enum Specifier {
-    Int(i32),
-    Str(String),
-    Float(f64),
-}
