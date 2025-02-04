96209eb4b4847dce1a2a186e8b6d79f5d3fc6d04
hill <hill@hilll.dev>
2025-02-04 02:49:03 +0100

    Update dependencies.sh

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,16 +3,20 @@
 set -e
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs -o rustup.sh
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -ssf -o wasmpack.sh
+curl -ssf https://rustwasm.github.io/wasm-pack/installer/init.sh -o wasmpack.sh
 curl -fsSL https://bun.sh/install -o bun.sh
 curl -fsSL https://get.pnpm.io/install.sh -o pnpm.sh
 
-chmod +x *.sh
+chmod +x rustup.sh wasmpack.sh bun.sh pnpm.sh
+
 ./rustup.sh --default-toolchain nightly --profile minimal -y
 rustup component add rustfmt clippy --toolchain nightly
 source "$HOME/.cargo/env"
+
 rm ~/.cargo/bin/rustfmt ~/.cargo/bin/rust-analyzer ~/.cargo/bin/cargo-fmt
+
 rustup update
+
 ./wasmpack.sh
 
 if [ $GITHUB_JOB == "update_and_lint" ]; then
