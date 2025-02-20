6487013d04a7962ad1654ea5eba2cb4f2f220d6d
hill <hill@hilll.dev>
2025-02-20 07:14:09 +0000

    generate

--- wasm/tuningplayground/generate_chords.py
+++ wasm/tuningplayground/generate_chords.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
 
     data = {}
     data2 = {}
+    data3 = {}
+
     for r in range(1, 13):
         combinations = itertools.combinations(range(12), r)
         for combination in combinations:
@@ -17,10 +19,12 @@ if __name__ == "__main__":
             bitmask = sum(1 << i for i in combination)
             data[bitmask] = c.pitchedCommonName
             data2[bitmask] = c.commonName
+            data3[bitmask] = c.fullName
 
     # sort the dictionary by key
     data = dict(sorted(data.items()))
     data2 = dict(sorted(data2.items()))
+    data3 = dict(sorted(data3.items()))
 
     with open("../../content/misc/plaintext/chords.txt", "w") as outfile:
         for key, value in data.items():
@@ -35,3 +39,10 @@ if __name__ == "__main__":
 
     with open("../../content/misc/plaintext/chords_unpitched.json", "w") as outfile:
         json.dump(data2, outfile, separators=(",", ":"))
+    
+    with open("../../content/misc/plaintext/chords_unpitched.txt", "w") as outfile:
+        for key, value in data3.items():
+            outfile.write(f"{value};")
+
+    with open("../../content/misc/plaintext/chords_unpitched.json", "w") as outfile:
+        json.dump(data3, outfile, separators=(",", ":"))
